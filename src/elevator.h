9ee9782d45cfb5f06f16b2321bd37a362b05915d
Atlas Mallams <131003995+atlaska826@users.noreply.github.com>
2024-10-12 22:53:41 -0500

    Basic elevator setup

--- src/elevator.h
+++ src/elevator.h
@@ -1,9 +1,26 @@
 #ifndef ELEVATORSIMULATION_ELEVATOR_H
 #define ELEVATORSIMULATION_ELEVATOR_H
 
+#include <queue>
+
+struct Passenger {
+    int initialFloor;
+    int destinationFloor;
+    int direction;
+    int timeEntered;
+
+    Passenger(int initial, int destination, int direct, int time)
+        : initialFloor(initial), destinationFloor(destination), direction(direct), timeEntered(time) {}
+};
 
 class Elevator {
-
+private:
+    std::queue<Passenger> elevatorQueue;
+    int initialFloor;
+    int destinationFloor; // May remove this later
+    int direction;
+    int numPassengers;
+    int capacity; // May remove this later
 };
 
 
