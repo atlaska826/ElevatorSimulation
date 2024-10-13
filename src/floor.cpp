98ab58e5cccd2436f29a9b462ce0b686a34d9866
Atlas Mallams <131003995+atlaska826@users.noreply.github.com>
2024-10-13 16:24:17 -0500

    Added comments

--- src/floor.cpp
+++ src/floor.cpp
@@ -1,15 +1,18 @@
 #include "floor.h"
 
+// Adds a passenger to the queue
 void Floor::addPassenger(Passenger passenger) {
     waitingPassengers.push(passenger);
 }
 
+// Removes and returns the next passenger in line
 Passenger Floor::getNextPassenger() {
     Passenger nextPassenger = waitingPassengers.front();
     waitingPassengers.pop();
     return nextPassenger;
 }
 
+// Checks if the floor has any waiting passengers
 bool Floor::hasWaitingPassengers() const {
     return !waitingPassengers.empty();
 }
--- src/floor.h
+++ src/floor.h
@@ -2,16 +2,23 @@
 #define ELEVATORSIMULATION_FLOOR_H
 
 #include "Passenger.h"
+
 #include <queue>
 
 class Floor {
-private:
-    std::queue<Passenger> waitingPassengers;
-
 public:
+    // Adds a passenger to the queue
     void addPassenger(Passenger passenger);
+
+    // Removes and returns the next passenger in line
     Passenger getNextPassenger();
+
+    // Checks if the floor has any waiting passengers
     bool hasWaitingPassengers() const;
+
+private:
+    // Queue of all passengers waiting on the floor
+    std::queue<Passenger> waitingPassengers;
 };
 
 #endif
