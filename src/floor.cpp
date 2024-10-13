bc7f5604586b7254eb8d57b67dcacb5649417aca
Atlas Mallams <131003995+atlaska826@users.noreply.github.com>
2024-10-13 16:18:46 -0500

    Added logic for managing floors.

--- src/floor.cpp
+++ src/floor.cpp
@@ -1,1 +1,15 @@
 #include "floor.h"
+
+void Floor::addPassenger(Passenger passenger) {
+    waitingPassengers.push(passenger);
+}
+
+Passenger Floor::getNextPassenger() {
+    Passenger nextPassenger = waitingPassengers.front();
+    waitingPassengers.pop();
+    return nextPassenger;
+}
+
+bool Floor::hasWaitingPassengers() const {
+    return !waitingPassengers.empty();
+}
--- src/floor.h
+++ src/floor.h
@@ -1,10 +1,17 @@
 #ifndef ELEVATORSIMULATION_FLOOR_H
 #define ELEVATORSIMULATION_FLOOR_H
 
+#include "Passenger.h"
+#include <queue>
 
 class Floor {
+private:
+    std::queue<Passenger> waitingPassengers;
 
+public:
+    void addPassenger(Passenger passenger);
+    Passenger getNextPassenger();
+    bool hasWaitingPassengers() const;
 };
-
 
 #endif
